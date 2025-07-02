81817eb8546b00660fe851a16c1115505b52394f
Gabriel-y60 <gpawlewski@gmail.com>
2025-07-02 21:02:14 +0200

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,3 +1,5 @@
+#Task: Print numbers from 1 to 100. But for multiples of 3 print "Fizz", for multiples of 5 print "Buzz", and for multiples of both, print "FizzBuzz".
+
 for i in range(1, 101):
     if i % 15 == 0:
         print("FizzBuzz")
@@ -7,4 +9,3 @@ for i in range(1, 101):
         print("Buzz")
     else:
         print(i)
-        
