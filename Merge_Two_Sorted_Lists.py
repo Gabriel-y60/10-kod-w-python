bed1e66bdbf70a9f3ad030f7f0688026b23da3a0
Gabriel-y60 <gpawlewski@gmail.com>
2025-07-02 21:03:06 +0200

    Update Merge_Two_Sorted_Lists.py

--- Merge_Two_Sorted_Lists.py
+++ Merge_Two_Sorted_Lists.py
@@ -1,5 +1,8 @@
+#Task: Merge two sorted lists into one sorted list.
+
 def merge_sorted_lists(a, b):
     return sorted(a + b)
 
+#example
 print(merge_sorted_lists([2,2,3,3,5], [1,1,2,2,4,4,6]))
 # Output: [1, 1, 2, 2, 2, 2, 3, 3, 4, 4, 5, 6]
