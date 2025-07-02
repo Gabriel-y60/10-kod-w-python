2731a7361f12cc1fb99d574b97283ceb6e7c20f2
Gabriel-y60 <gpawlewski@gmail.com>
2025-07-02 21:08:41 +0200

    Update second_largest.py

--- second_largest.py
+++ second_largest.py
@@ -1,5 +1,10 @@
+#Task: Write a function to find the second largest number in a list
+
 def second_largest(lst):
     unique = set(lst)
     unique.discard(max(unique))
     return max(unique)
+
+#example
 print(second_largest([200,300,400,500,600,700]))
+#output: 
