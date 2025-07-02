5a45b1ffb190919a4ab198964384d85fcf8beec3
Gabriel-y60 <gpawlewski@gmail.com>
2025-07-02 20:59:04 +0200

    Update Find_Number.py

--- Find_Number.py
+++ Find_Number.py
@@ -1,6 +1,9 @@
+#Task: Given a list of numbers from 1 to n with one number missing, find the missing number.
+
 def find_missing_number(lst):
     n = len(lst) + 1
     return n * (n + 1) // 2 - sum(lst)
 
+#example
 print(find_missing_number([1,2,3,4,6,7,8,9]))
 # Output: 5
