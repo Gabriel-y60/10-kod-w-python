f99350fd3be701d105326e27fe687c01caa8f589
Gabriel-y60 <gpawlewski@gmail.com>
2025-07-02 21:05:58 +0200

    Update Reverse_Words.py

--- Reverse_Words.py
+++ Reverse_Words.py
@@ -1,6 +1,9 @@
+#Task: Write a function that takes a sentence as input and returns the sentence with the word order reversed.
+
 def reverse_sentence(sentence):
     return ' '.join(sentence.split()[::-1])
 
+#example
 print(reverse_sentence("nissan patrol is cool"))
 # Output: "cool is patrol nissan"
 
