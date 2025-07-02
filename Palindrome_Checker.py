4830cc120509e7a39101a8323a2505c1a17e26dc
Gabriel-y60 <gpawlewski@gmail.com>
2025-07-02 21:04:15 +0200

    Update Palindrome_Checker.py

--- Palindrome_Checker.py
+++ Palindrome_Checker.py
@@ -1,6 +1,9 @@
+#Task: Check whether a given string is a palindrome (reads the same forward and backward).
+
 def is_palindrome(s):
     return s == s[::-1]
 
+#example
 print(is_palindrome("kajak"))
 # Output: True
 print(is_palindrome("wheel"))
