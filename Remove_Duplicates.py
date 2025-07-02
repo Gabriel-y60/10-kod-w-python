60e4014bc53460a1591dfa0c9ad32a270b2d7377
Gabriel-y60 <gpawlewski@gmail.com>
2025-07-02 21:05:03 +0200

    Update Remove_Duplicates.py

--- Remove_Duplicates.py
+++ Remove_Duplicates.py
@@ -1,3 +1,5 @@
+#Task: Write a function to remove duplicates from a list without using set() directly in the return statement.
+
 def remove_duplicates(lst):
     result = []
     for item in lst:
@@ -5,5 +7,6 @@ def remove_duplicates(lst):
             result.append(item)
     return result
 
+#example
 print(remove_duplicates([1,1,2,3,3,3,4,5,6,6,77,77,8]))
 # Output: [1, 2, 3, 4, 5, 6, 77, 8]
